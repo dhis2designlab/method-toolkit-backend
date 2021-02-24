b4ab90d1c36eba5861b3396e0720a9cc41f0cbfa
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-02-24 14:42:19 +0100

    #10 #13 #14 Added new fields, sub-activities and single-type for about page

--- api/technique/models/technique.js
+++ api/technique/models/technique.js
@@ -1,23 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
- const slugify = require('slugify');
+const slugify = require("slugify");
+const Boom = require("Boom");
+
+const generateSlug = (data) => {
+  if (data.title) {
+    data.slug = slugify(data.title.toLowerCase());
+  }
+};
+
+const isMinimumLarger = (min, max) => {
+  return min > max;
+};
+
+const validateMinimumTime = (data) => {
+  if (data.minimum_time && data.maximum_time) {
+    const minimum = data.minimum_time;
+    const maximum = data.maximum_time;
+
+    if (isMinimumLarger(minimum, maximum)) {
+      throw Boom.badRequest(
+        "The maximum value cant't be lower than the minimum value."
+      );
+    }
+  }
+};
 
 module.exports = {
-    lifecycles: {
-        beforeCreate: async (data) => {
-            if (data.title) {
-                data.slug = slugify(data.title);
-            }
-        },
-        beforeUpdate: async (params, data) => {
-            if (data.title) {
-                data.slug = slugify(data.title);
-            }
-        }
-    }
+  lifecycles: {
+    beforeCreate: async (data) => {
+      generateSlug(data);
+      validateMinimumTime(data);
+    },
+    beforeUpdate: async (params, data) => {
+      generateSlug(data);
+      validateMinimumTime(data);
+    },
+  },
 };
