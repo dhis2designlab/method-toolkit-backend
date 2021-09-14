e2ec09894020ea8a0364cba6dc0b1a155796c776
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-09-14 16:45:28 +0200

    X switch production and development config

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,16 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        uri: env('DATABASE_URI')
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE') || null,
-        ssl: env.bool('DATABASE_SSL'),
+        ssl: true,
       },
     },
   },
